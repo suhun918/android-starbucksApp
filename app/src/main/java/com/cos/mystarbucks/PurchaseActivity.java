30d9d5f02b50d3ceaef8c5e79aea2dd4a2254cfd
suhun918 <suhun918@naver.com>
2020-01-14 15:37:29 +0900

    구매페이지 디자인 및 수량증가기능

--- app/src/main/java/com/cos/mystarbucks/PurchaseActivity.java
+++ app/src/main/java/com/cos/mystarbucks/PurchaseActivity.java
@@ -7,26 +7,25 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cos.mystarbucks.model.User;
+import com.cos.mystarbucks.util.Localhost;
 import com.cos.mystarbucks.util.NavigationFactory;
 import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
 
 public class PurchaseActivity extends AppCompatActivity {
     private Toolbar toolbar;
-    private ImageView menuIcon;
-
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-
-    private View header;
-    private Button btnLogin;
-
-    private TextView name, price, img;
+    private ImageView ivImg;
+    private TextView tvName, tvPrice, tvCount;
+    private Button btnIncrease, btnDecrease, btnCart, btnOrder;
+    private int count = 1, price, ePrice;
+    private String name, img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,63 +33,86 @@ public class PurchaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_purchase);
 
         minit();
-        NavigationFactory.setNavigation(getApplicationContext(), drawerLayout, navigationView, header, btnLogin);
         toolbarSetting();
-
         receiveData();
+        quantity();
     }
 
     private void minit(){
-        toolbar = findViewById(R.id.toolbar);
-        menuIcon = findViewById(R.id.menu_icon);
+        toolbar = findViewById(R.id.toolbarPurchase);
+        btnCart = findViewById(R.id.btn_cart);
+        btnOrder = findViewById(R.id.btn_order);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigationView);
 
-        header = navigationView.getHeaderView(0);
-        btnLogin = header.findViewById(R.id.btn_login);
     }
 
     private void toolbarSetting(){
         setSupportActionBar(toolbar);
-        menuIcon.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);//검정화살표가 나오길래 내가 집어넣는 하얀 화살표
+        }
 
+    //툴바버튼 클릭 이벤트
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){//여기서 버튼별로 인텐트도 가능
+            case android.R.id.home:{//toolbar의 back키를 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void receiveData(){
-        name = findViewById(R.id.tv_name);
-        price = findViewById(R.id.tv_price);
-        img = findViewById(R.id.tvimg);
+        tvName = findViewById(R.id.tv_purName);
+        tvPrice = findViewById(R.id.tv_purPrice);
+        ivImg = findViewById(R.id.iv_purImg);
 
         Intent intent = getIntent();
 
-        String cName = intent.getExtras().getString("name");
-        name.setText(cName);
+        name = intent.getExtras().getString("name");
+        tvName.setText(name);
 
-        int cPrice = intent.getExtras().getInt("price");
-        price.setText(String.valueOf(cPrice));
+        price = intent.getExtras().getInt("price");
+        tvPrice.setText(price+"원");
 
-        String cImg = intent.getExtras().getString("img");
-        img.setText(cImg);
+        img = intent.getExtras().getString("img");
+        Picasso.get()
+                .load(img)
+                .into(ivImg);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        User user = User.getInstance();
-        TextView tv = header.findViewById(R.id.tv_name);
-        if(user.getId() != 0){
-            btnLogin.setText("로그아웃");
-            tv.setText(user.getName());
-            tv.setVisibility(View.VISIBLE);
-        }else{
-            btnLogin.setText("로그인");
-            tv.setVisibility(View.GONE);
-        }
+    private void quantity(){
+        btnIncrease = findViewById(R.id.btn_plus);
+        btnDecrease = findViewById(R.id.btn_minus);
+        tvCount = findViewById(R.id.tv_quantity);
+        tvCount.setText(count+"");
+
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(count<20){
+                    count = count+1;
+                    tvCount.setText(count+"");
+                    tvPrice.setText(count*price+"원");
+
+                }
+            }
+        });
+
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(count>1){
+                    count = count-1;
+                    tvCount.setText(count+"");
+                    tvPrice.setText(count*price+"원");
+                }
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/cos/mystarbucks/util/Localhost.java
+++ app/src/main/java/com/cos/mystarbucks/util/Localhost.java
@@ -1,5 +1,5 @@
 package com.cos.mystarbucks.util;
 
 public interface Localhost {
-    String URL = "http://222.234.36.87:1000";
+    String URL = "http://192.168.0.50:8080";
 }
